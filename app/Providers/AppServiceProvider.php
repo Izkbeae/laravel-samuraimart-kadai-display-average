74cae108ea6383d7fe5c12a7ab468bb108a693cd
阿部瑞樹 <abemizuki@abemizukinoMacBook-Air.local>
2024-08-21 09:20:02 +0900

    commit

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,6 +4,8 @@ namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Pagination\Paginator;
+use Illuminate\Support\Facades\App;
+use Illuminate\Support\Facades\URL;
 
 
 class AppServiceProvider extends ServiceProvider
@@ -27,6 +29,10 @@ class AppServiceProvider extends ServiceProvider
     {
         //
         Paginator::useBootstrap();
+        if (App::environment(['production'])) {
+            URL::forceScheme('https');
+        }
+
 
     }
 }
